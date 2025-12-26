77e51b280ff2b3a9e255284b817fd9f0740f944a
JQRG <165354267+JimmyQrg@users.noreply.github.com>
2025-12-25 22:38:29 -0800

    -

---
+++ dark_mode.js
@@ -0,0 +1,10 @@
+const body = document.body;
+
+// Turn on dark mode
+body.classList.add('dark');
+
+// Turn off dark mode
+body.classList.remove('dark');
+
+// Toggle
+body.classList.toggle('dark');
